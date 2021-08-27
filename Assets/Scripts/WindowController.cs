c455c62f74f59f46a85acb7c7479ee62e12f4361
Fernando Guillen <fguillen.mail@gmail.com>
2021-08-27 20:11:16 +0200

    Grabbable: Windows and Cars explode

--- Assets/Scripts/WindowController.cs
+++ Assets/Scripts/WindowController.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class WindowController : MonoBehaviour
 {
     [SerializeField] GameObject windowBrokenPrefab;
+    [SerializeField] Color burntColor = new Color(0.27f, 0.076f, 0.15f);
+    [SerializeField] SpriteRenderer colorizable;
+    [SerializeField] ParticleSystem particlesExplosion;
+    [SerializeField] ParticleSystem particlesSmoke;
 
     public void StartGrab()
     {
@@ -17,5 +22,24 @@ public class WindowController : MonoBehaviour
     public void Thrown()
     {
         Instantiate(windowBrokenPrefab, transform.position, Quaternion.identity, transform.parent);
+    }
+
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        if(
+            collisionInfo.gameObject.CompareTag("WindowGround")
+        )
+        {
+            StartCoroutine(ExplodeCoroutine());
+        }
+    }
+
+    IEnumerator ExplodeCoroutine()
+    {
+        Debug.Log("Car.Explode()");
+        particlesExplosion.Play();
+        particlesSmoke.Play();
+
+        yield return colorizable.DOColor(burntColor, 0.5f).WaitForCompletion();
     }
 }
