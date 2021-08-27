b84f0501c0b2ede8fda40e4b9b3f14beb220acc6
Gabriel Gallardo <pekena.larva@gmail.com>
2021-08-27 18:55:01 +0200

    New (better) audio manager

---
+++ Assets/Scripts/AudioType.cs
@@ -0,0 +1,18 @@
+﻿
+namespace UnityCore {
+
+    namespace Audio {
+
+        public enum AudioType {
+            None,
+            SFX_01,
+            SFX_02,
+            SFX_03,
+            SFX_04,
+            SFX_05,
+            
+            BCKGR_01,
+            BCKGR_02
+        }
+    }
+}
