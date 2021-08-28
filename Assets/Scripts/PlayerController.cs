32e608ab0d9febe7888f470a6673d1c294b29b84
Fernando Guillen <fguillen.mail@gmail.com>
2021-08-28 19:51:07 +0200

    Tentacles: Raycast implementation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    [SerializeField] float maxTentacleDistance = 5.0f;
+    [SerializeField] List<Transform> tentacleTargets;
+    [SerializeField] LayerMask tentacleTargetLayers;
+
     // [SerializeField] int numPoints = 3;
     // [SerializeField] float radius = 1.0f;
 
@@ -24,4 +29,47 @@ public class PlayerController : MonoBehaviour
     //         }
     //     }
     // }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            ShootTentacle();
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        var result = RaycastTentacle();
+
+        Gizmos.color = Color.grey;
+        Vector2 finalPosition =  (Vector2)transform.position + (result.direction * maxTentacleDistance);
+        Gizmos.DrawLine(transform.position, finalPosition);
+
+        // Debug.Log($"direction: {result.direction}");
+
+        if(result.hit)
+        {
+            Gizmos.DrawSphere(result.hit.transform.position, 0.5f);
+        }
+    }
+
+    void ShootTentacle()
+    {
+        RaycastHit2D hit = RaycastTentacle().hit;
+
+        if(hit)
+        {
+            hit.collider.gameObject.GetComponent<GrabbableController>().StartGrab();
+        }
+    }
+
+    (Vector2 direction, RaycastHit2D hit) RaycastTentacle()
+    {
+        Vector3 mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mousePosition - transform.position).normalized;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, maxTentacleDistance, tentacleTargetLayers);
+
+        return (direction: direction, hit: hit);
+    }
 }
