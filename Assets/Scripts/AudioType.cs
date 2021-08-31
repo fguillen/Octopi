a13151cbd27c06b0b3889c08fb314deda9eb8c3a
Gabriel Gallardo <pekena.larva@gmail.com>
2021-08-31 20:28:55 +0200

    TestScript para sonido
    Tambien arreglé el tema de corutinas en el audio manager

--- Assets/Scripts/AudioType.cs
+++ Assets/Scripts/AudioType.cs
@@ -4,20 +4,31 @@ namespace UnityCore {
     namespace Audio {
 
         public enum AudioType {
+          
             None,
 
             SFX_grabObject,
-            SFX_windowPull,
+            SFX_windowPull_01,
+            SFX_windowPull_02,
+            SFX_windowPull_03,
+            SFX_windowPull_04,
+            SFX_windowPull_05,
+            SFX_windowPull_06,
             SFX_windowCrash,
             SFX_fire,
 
             SFX_bulletShoot,
             SFX_missileShoot,
-            SFX_bulletImpact,
+            SFX_bulletImpact_01,
+            SFX_bulletImpact_02,
+            SFX_bulletImpact_03,
+            SFX_bulletImpact_04,
+            SFX_bulletImpact_05,
             SFX_missileImpact,
             SFX_missileExplosion,
             SFX_tankTurret,
 
+            SFX_birdGrab,
             SFX_birdCrash,
             SFX_carCrash,
             SFX_airplaneCrash,
@@ -27,9 +38,9 @@ namespace UnityCore {
             BCKGR_city,
             BCKGR_beach,
 
-            MUS_menu,
             MUS_level,
             MUS_stinger,
+            //...custom audio types here
         }
     }
 }
--- Assets/Scripts/TestAudio.cs
+++ Assets/Scripts/TestAudio.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace UnityCore {
 
-    /*namespace Audio {
+    namespace Audio {
 
         public class TestAudio : MonoBehaviour
         {
@@ -13,27 +13,111 @@ namespace UnityCore {
 #region Unity Functions
 #if UNITY_EDITOR
             private void Update() {
-                if (Input.GetKeyUp(KeyCode.T)) {
-                    audioController.PlayAudio(AudioType.ST_01, true); //bool is for fades (yes/no)
+
+                //Grab and windows
+                if (Input.GetKeyDown(KeyCode.Alpha1)) {
+                    audioController.PlayAudio(AudioType.SFX_grabObject, false); //bool is for fades (yes/no)
                 }
-                if (Input.GetKeyUp(KeyCode.G)) {
-                    audioController.StopAudio(AudioType.ST_01, true);
+                if (Input.GetKeyDown(KeyCode.Alpha2)) {
+                    audioController.PlayAudio(AudioType.SFX_windowPull_01, false);
                 }
-                if (Input.GetKeyUp(KeyCode.B)) {
-                    audioController.RestartAudio(AudioType.ST_01, true);
+                if (Input.GetKeyDown(KeyCode.Alpha3)) {
+                    audioController.PlayAudio(AudioType.SFX_windowCrash, false);
                 }
-                if (Input.GetKeyUp(KeyCode.Y)) {
-                    audioController.PlayAudio(AudioType.SFX_01, true);
+
+                //fire on/off
+                if (Input.GetKeyDown(KeyCode.Alpha4)) {
+                    audioController.PlayAudio(AudioType.SFX_fire, false);
                 }
-                if (Input.GetKeyUp(KeyCode.H)) {
-                    audioController.StopAudio(AudioType.SFX_01, true);
+                if (Input.GetKeyDown(KeyCode.Alpha4))
+                {
+                    audioController.StopAudio(AudioType.SFX_fire, false);
                 }
-                if (Input.GetKeyUp(KeyCode.N)) {
-                    audioController.RestartAudio(AudioType.SFX_01, true);
+
+                //guns
+                if (Input.GetKeyUp(KeyCode.Alpha5)) {
+                    audioController.PlayAudio(AudioType.SFX_bulletShoot, false);
+                }
+                if (Input.GetKeyDown(KeyCode.Alpha6))
+                {
+                    audioController.PlayAudio(AudioType.SFX_bulletImpact_01, false);
+                }
+                if (Input.GetKeyDown(KeyCode.Alpha7)) {
+                    audioController.PlayAudio(AudioType.SFX_missileShoot, false);
+                }
+                if (Input.GetKeyDown(KeyCode.Alpha8))
+                {
+                    audioController.PlayAudio(AudioType.SFX_missileImpact, false);
+                }
+                if (Input.GetKeyDown(KeyCode.Alpha9))
+                {
+                    audioController.PlayAudio(AudioType.SFX_missileExplosion, false);
+                }
+                if (Input.GetKeyDown(KeyCode.Alpha0))
+                {
+                    audioController.PlayAudio(AudioType.SFX_tankTurret, false);
+                }
+
+                //Bird
+                if (Input.GetKeyDown(KeyCode.T))
+                {
+                    audioController.PlayAudio(AudioType.SFX_birdGrab, false);
+                }
+                if (Input.GetKeyUp(KeyCode.T))
+                {
+                    audioController.PlayAudio(AudioType.SFX_birdCrash, false);
+                }
+
+                //Vehicle crashes
+                if (Input.GetKeyDown(KeyCode.Y))
+                {
+                    audioController.PlayAudio(AudioType.SFX_carCrash, false);
+                }
+                if (Input.GetKeyDown(KeyCode.U))
+                {
+                    audioController.PlayAudio(AudioType.SFX_airplaneCrash, false);
+                }
+
+                //UI
+                if (Input.GetKeyDown(KeyCode.I))
+                {
+                    audioController.PlayAudio(AudioType.SFX_ui, false);
+                }
+
+                //Background loops
+                if (Input.GetKeyDown(KeyCode.O))
+                {
+                    audioController.PlayAudio(AudioType.BCKGR_city, true);
+                }
+                if (Input.GetKeyDown(KeyCode.P))
+                {
+                    audioController.StopAudio(AudioType.BCKGR_city, true);
+                }
+                if (Input.GetKeyDown(KeyCode.G))
+                {
+                    audioController.PlayAudio(AudioType.BCKGR_beach, true);
+                }
+                if (Input.GetKeyDown(KeyCode.H))
+                {
+                    audioController.StopAudio(AudioType.BCKGR_beach, true);
+                }
+
+                //music
+                if (Input.GetKeyDown(KeyCode.J))
+                {
+                    audioController.PlayAudio(AudioType.MUS_level, false);
+                }
+                if (Input.GetKeyDown(KeyCode.K))
+                {
+                    audioController.StopAudio(AudioType.MUS_level, true);
+                }
+                if (Input.GetKeyDown(KeyCode.L))
+                {
+                    audioController.PlayAudio(AudioType.MUS_stinger, false);
                 }
             }
 #endif
 #endregion
         }
-    }*/
+    }
 }
