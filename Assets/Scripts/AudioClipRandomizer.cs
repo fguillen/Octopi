e90bf923ca5900483748f17ad302adc4a1c57801
Gabriel Gallardo <pekena.larva@gmail.com>
2021-09-01 02:03:58 +0200

    Clip randomization finished

---
+++ Assets/Scripts/AudioClipRandomizer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityCore
+{
+    namespace Audio
+    {
+        public class AudioClipRandomizer : MonoBehaviour
+        {
+            public TestAudio testAudio;
+
+            // Start is called before the first frame update
+            void Start()
+            {
+        
+            }
+        }
+    }
+}
--- Assets/Scripts/AudioType.cs
+++ Assets/Scripts/AudioType.cs
@@ -8,33 +8,43 @@ namespace UnityCore {
             None,
 
             SFX_grabObject,
+
             SFX_windowPull_01,
             SFX_windowPull_02,
             SFX_windowPull_03,
             SFX_windowPull_04,
             SFX_windowPull_05,
             SFX_windowPull_06,
+            SFX_windowPull_07,
+            SFX_windowPull_08,
+
             SFX_windowCrash_01,
             SFX_windowCrash_02,
             SFX_windowCrash_03,
+
             SFX_fire,
 
             SFX_bulletShoot,
             SFX_missileShoot,
+
             SFX_bulletImpact_01,
             SFX_bulletImpact_02,
             SFX_bulletImpact_03,
             SFX_bulletImpact_04,
             SFX_bulletImpact_05,
+
             SFX_missileImpact,
             SFX_missileExplosion,
+
             SFX_tankTurret,
 
             SFX_birdGrab,
             SFX_birdCrash,
+
             SFX_carCrash_01,
             SFX_carCrash_02,
             SFX_carCrash_03,
+
             SFX_airplaneCrash,
 
             SFX_ui,
@@ -44,6 +54,7 @@ namespace UnityCore {
 
             MUS_militaryMarch,
             MUS_win,
+
             //...custom audio types here
         }
     }
