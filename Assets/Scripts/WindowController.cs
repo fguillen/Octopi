3abec7c68461b9580b747aecb31b1bcbb25e150f
Fernando Guillen <fguillen.mail@gmail.com>
2021-09-01 10:46:46 +0200

    Audio: Windows crash

--- Assets/Scripts/WindowController.cs
+++ Assets/Scripts/WindowController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityCore.Audio;
 
 public class WindowController : MonoBehaviour
 {
@@ -29,6 +30,7 @@ public class WindowController : MonoBehaviour
             onWall = false;
         }{
             particlesExplosion.Play();
+            PlayThrowSound();
         }
     }
 
@@ -48,8 +50,60 @@ public class WindowController : MonoBehaviour
         particlesExplosion.Play();
         particlesSmoke.Play();
 
+        PlayCrashSound();
+
         gameObject.layer = LayerMask.NameToLayer("WindowsHidden");
 
         yield return colorizable.DOColor(burntColor, 0.5f).WaitForCompletion();
+    }
+
+    void PlayThrowSound()
+    {
+        int windowPullClip = Random.Range(1, 9);
+
+        switch (windowPullClip)
+        {
+            case 1:
+                AudioController.instance.PlayAudio(UnityCore.Audio.AudioType.SFX_windowPull_01, false);
+                break;
+            case 2:
+                AudioController.instance.PlayAudio(UnityCore.Audio.AudioType.SFX_windowPull_02, false);
+                break;
+            case 3:
+                AudioController.instance.PlayAudio(UnityCore.Audio.AudioType.SFX_windowPull_03, false);
+                break;
+            case 4:
+                AudioController.instance.PlayAudio(UnityCore.Audio.AudioType.SFX_windowPull_04, false);
+                break;
+            case 5:
+                AudioController.instance.PlayAudio(UnityCore.Audio.AudioType.SFX_windowPull_05, false);
+                break;
+            case 6:
+                AudioController.instance.PlayAudio(UnityCore.Audio.AudioType.SFX_windowPull_06, false);
+                break;
+            case 7:
+                AudioController.instance.PlayAudio(UnityCore.Audio.AudioType.SFX_windowPull_07, false);
+                break;
+            case 8:
+                AudioController.instance.PlayAudio(UnityCore.Audio.AudioType.SFX_windowPull_08, false);
+                break;
+        }
+    }
+
+    void PlayCrashSound()
+    {
+        int windowCrashClip = Random.Range(1, 4);
+        switch (windowCrashClip)
+        {
+            case 1:
+                AudioController.instance.PlayAudio(UnityCore.Audio.AudioType.SFX_windowCrash_01, false);
+                break;
+            case 2:
+                AudioController.instance.PlayAudio(UnityCore.Audio.AudioType.SFX_windowCrash_02, false);
+                break;
+            case 3:
+                AudioController.instance.PlayAudio(UnityCore.Audio.AudioType.SFX_windowCrash_03, false);
+                break;
+        }
     }
 }
